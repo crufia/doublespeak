45620d10856f68f7f360f913f9d20fe2ec535a20
Dave Kincade <dave@jiggy.ca>
2016-01-22 19:47:34 -0800

    Basic markov chains for data from redstate and timecube

--- lib/doublespeak.rb
+++ lib/doublespeak.rb
@@ -1,5 +1,14 @@
 require 'doublespeak/version'
+require 'marky_markov'
 
 module Doublespeak
-  # Your code goes here...
+  def self.generate_timecube(size = 5)
+    @timecube ||= MarkyMarkov::TemporaryDictionary.new.tap { |m| m.parse_file 'lib/texts/timecube.txt' }
+    @timecube.generate_n_sentences size
+  end
+
+  def self.generate_redstate(size = 5)
+    @redstate ||= MarkyMarkov::TemporaryDictionary.new.tap { |m| m.parse_file 'lib/texts/redstate.txt' }
+    @redstate.generate_n_sentences size
+  end
 end
